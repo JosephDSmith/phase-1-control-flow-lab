85dfbad94f847f2238108dfa439bd4c763b1c297
Joseph Smith <joesmith40@gmail.com>
2023-02-21 20:38:39 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  if (n <= 400) {
+    return `This one is on me!`;
+  } else if (n > 400 & n <2000) {
+    return `That will be twenty bucks.`;
+  } else if (n > 2000 & n <= 2500) {
+    return `I will gladly take your thirty bucks.`;
+  } else {
+    return `No can do.`;
+  }
+  }
+
+function ternaryCheckCity(d){
+  return d===`NYC`?`Ok, sounds good.` : `No go.`
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  switch(t){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  } 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
